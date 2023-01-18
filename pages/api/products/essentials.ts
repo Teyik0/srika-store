71c2efd6eba612d3746d78e80473f9d19980c747
Teyik0 <teyik0.fr@icloud.com>
2023-01-18 22:24:49 +0100

    test

--- pages/api/products/essentials.ts
+++ pages/api/products/essentials.ts
@@ -1,9 +1,12 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { client, essentialProductsQuery } from '../../../utils/utils';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { client, allProductsQuery } from '../../../utils/utils';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if(req.method === 'GET') {
-    const query = essentialProductsQuery();
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'GET') {
+    const query = allProductsQuery();
     const data = await client.fetch(query);
     res.status(200).json(data);
   }
